f00cae94e8b9470b64d348339253ab019d60bd9d
samuel1346 <samielnsilva@gmail.com>
2024-02-23 22:32:49 -0300

    my resume

---
+++ jsFolder/experience.js
@@ -0,0 +1,11 @@
+const main = document.getElementById('main')
+const printMain = document.createElement('div')
+printMain.innerHTML= 
+`
+    <div>
+        <h1>
+            experiences
+        </h1>
+    </div>
+`
+main.appendChild(printMain)
---
+++ jsFolder/presentation.js
@@ -0,0 +1,53 @@
+const header = document.getElementById('header');
+const body = document.getElementById('body');
+const printPresentation = document.createElement('div');
+printPresentation.innerHTML = 
+`
+<div class="headPresentation">
+    <div class="headSection" >
+        <div>
+            <img class="headPresentationImg" id="headPresentationImg" src="https://media.licdn.com/dms/image/D4D03AQG5ZKHj1ygxFw/profile-displayphoto-shrink_800_800/0/1708717242300?e=1714003200&v=beta&t=oEels292KULkY30rQsqmWUwQgnEA3AXHNEsocaqbnZg" alt="">
+        </div>
+        <div class="headPresentationInformation">  
+            <p>    Name: "Samuel Nicolas"</p>
+            <p>    Surname: "Silva"</p>
+            <p>    Age: "24 years"</p>
+            <p>    Passport: "AAF259541"</p>
+            <p>    Email: "samielnsilva@gmail.com"</p>
+            <p>    Linkedin: "https://www.linkedin.com/in/samuel-silva-17b6011b8/"
+        </div>  
+    </div>
+        <button class="themeButton" id= 'themeButton'>
+            <img class="themeButtonImg" src="https://cdn0.iconfinder.com/data/icons/display-setting/64/Choose_theme-512.png">
+        </button>
+</div>
+`
+header.appendChild(printPresentation);
+
+const headPresentationImg = document.getElementById('headPresentationImg')
+const themeButton = document.getElementById('themeButton')
+
+    function changeTheme(){
+        themeButton.addEventListener('click', ()=>{
+            if(!body.classList[1]){
+                body.classList.add('changeTheme')
+            } else if(body.classList[1]){
+                body.classList.remove('changeTheme')
+            }
+        })
+    }
+    
+    function showProfile(){
+        headPresentationImg.addEventListener("click", ()=>{
+            if(!headPresentationImg.classList[1]){
+                headPresentationImg.classList.add('showProfile')
+            }else if(headPresentationImg.classList[1]){
+                headPresentationImg.classList.remove('showProfile')
+            }
+        })
+    }
+    changeTheme()
+    showProfile()
+
+
+
